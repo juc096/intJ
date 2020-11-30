5ef0bc0b3d8732b466871dcc3c89995c0a27e7ff
juc096 <juc096@ucsd.edu>
2020-11-30 11:05:55 -0800

    while loop pract

--- src/11_30.kt
+++ src/11_30.kt
@@ -18,4 +18,42 @@ fun main(args: Array<String>) {
         else -> println("not a teenager")
     }
 
+    var p = 0
+    var v = 1
+    while(p < 10) {
+        println("2 to power of $p : $v")
+        v *= 2
+        p += 1
+    }
+
+    println(calcDays("february", 2020))
+
+    println(fact(4))
+
+
+}
+
+fun calcDays (mon: String, year: Int) : Int {
+    when(mon.toLowerCase()) {
+
+        "february" -> {
+            return if ((year % 4 == 0) && (year % 100 != 0)) 29 else 28
+        }
+
+        "january", "march", "may", "july", "august", "october", "december"
+            -> return 31
+
+    }
+    return 30
+}
+
+fun fact(n : Int) : Int {
+    var f = 1
+    var it = 0
+    while (it < n) {
+        it += 1
+        f *= it
+    }
+    return f
+
 }
