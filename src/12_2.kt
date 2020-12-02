76edb759a2857d0cdea139f48f00557a1d208e1e
juc096 <juc096@ucsd.edu>
2020-12-02 12:14:19 -0800

    12_2_2020 exercises

---
+++ src/12_2.kt
@@ -0,0 +1,51 @@
+import java.lang.NullPointerException
+
+fun printFullName(firstname: String, lastname: String) : Unit {
+    println("$firstname $lastname")
+}
+fun calculateFullName(firstname: String, lastname: String) : Pair<String, Int> {
+    val temp = "$firstname $lastname"
+    return Pair(temp, temp.length-1)
+}
+tailrec fun fibo(n : Int) : Int {
+    if (n <= 2)
+        return 1
+    return fibo(n-2) + fibo(n-1)
+}
+fun add(n: Int, m: Int) : Int {
+    return n + m
+}
+fun sub(n: Int, m: Int) : Int {
+    return n - m
+}
+
+fun arithmeticOp(n: Int, m: Int, op: (Int, Int) -> Int) : Int {
+    return op(n,m)
+}
+
+fun main(args: Array<String>) {
+
+    printFullName("Justin", "Chen")
+
+    val myName_length = calculateFullName("Justin", "Chen")
+    println("Name: ${myName_length.first} Length: ${myName_length.second}")
+
+    println(fibo(12))
+
+    //arithmetic operation function expects a functional type
+    //use method reference operator
+    println(arithmeticOp(1, 2, ::add))
+    println(arithmeticOp(1, 2, ::sub))
+
+    val myFavoriteSong : String? = null
+
+    try {
+        println(myFavoriteSong!!.length)
+    }
+    catch (e : NullPointerException) {
+        println("Null song value")
+    }
+
+    println("Str length: ${myFavoriteSong?.length ?: "Null song value"}")
+
+}
